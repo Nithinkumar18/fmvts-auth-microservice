4c22c73f56046744e08d4ef95b8e78b573eeb809
Nithin <vooturinithinkumar@gmail.com>
2025-04-09 16:05:15 +0530

    add constants and responses

---
+++ auth/constants/responseInfo.js
@@ -0,0 +1,18 @@
+const info = {
+
+   INVALID_EMAIL: "Invalid email, Please try again with valid email",
+   INVALID_PASSWORD: "Incorrect Password, Please provide valid password to process further",
+   LOGIN_SUCCESS: "User login success!",
+   INVALID_USER_CREDENTIALS:"Please provide valid  user details like  email,password",
+   INCOMPLETE_USERDATA: "Please provide complete user details for user regestration",
+   ERR_LOGIN: "Error validating user credentials!",
+   USER_REGESTRATION: "User regestration success",
+   ERR_USER_REGESTRATION: "We're currently experiencing a server issue and are unable to register your account at the moment. Please try again later",
+   TOKEN_UNDEFINED: "Authentication token missing or improperly formatted. Please log in and try again.",
+   TOKEN_EXPIRED: "Your authentication token has expired or is malformed. Please log in again to continue.",
+   ACCESS_DENIED: "You do not have the required permissions to perform this action; please contact support or an administrator for assistance if you believe this is an error",
+   ERROR_VALIDATING_ROLE: "Role validation failed due to issue at server Please try again after some time"
+
+}
+
+module.exports = info;
---
+++ auth/constants/statusConstants.js
@@ -0,0 +1,10 @@
+const httpConstants = {
+    SUCCESS: 200,
+    CREATED: 201,
+    NOT_FOUND: 404,
+    INTERNAL_SERVER_ERROR: 500,
+    UNAUTHORIZED: 401,
+    FORBIDDEN: 403,
+    BAD_REQUEST: 400
+}
+module.exports =  httpConstants

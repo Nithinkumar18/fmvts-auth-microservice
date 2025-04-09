920b48e7767b75f222baa23fbb70a531e9bbf30b
Nithin <vooturinithinkumar@gmail.com>
2025-04-09 16:11:38 +0530

    update server

--- auth/server.js
+++ auth/server.js
@@ -1,4 +1,13 @@
 
 const express = require('express');
+require('dotenv').config();
 
 const app = express();
+const authRoutes = require('./routes/authRoutes');
+
+app.use(express.json());
+app.use('/auth/v1',authRoutes);
+const PORT = process.env.PORT;
+app.listen(PORT,() => {
+    console.log(`Auth-MicroService started on PORT ${PORT}`);
+})

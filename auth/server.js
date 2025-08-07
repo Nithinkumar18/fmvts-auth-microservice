9ea275b6e1351dabad89ff57cf49540e9d5dd720
Nithin <vooturinithinkumar@gmail.com>
2025-08-07 19:26:16 +0530

    update baseURL

--- auth/server.js
+++ auth/server.js
@@ -8,7 +8,7 @@ const app = express();
 const authRoutes = require('./src/routes/authRoutes');
 
 app.use(express.json());
-app.use('/v1',authRoutes);
+app.use('/v1/auth',authRoutes);
 const PORT = process.env.PORT;
 app.listen(PORT,() => {
     logger.info(`${info.SERVICE_NAME} started on PORT ${PORT} 🔑`);
